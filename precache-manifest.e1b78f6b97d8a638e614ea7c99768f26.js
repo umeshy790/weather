82bbe38f9cd4d93149044ddc850f5ce18e46c3d4
Umesh <umeshy790@gmail.com>
2019-11-30 17:35:00 +0530

    Updates

---
+++ precache-manifest.e1b78f6b97d8a638e614ea7c99768f26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8130c0750fee797bfdbf641ddf5499ea",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "9cf6e53af03c4a0181e9",
+    "url": "/weather/static/css/main.fdc17bad.chunk.css"
+  },
+  {
+    "revision": "d5bc35cd1e92f4fa3834",
+    "url": "/weather/static/js/2.79e0ec8c.chunk.js"
+  },
+  {
+    "revision": "9cf6e53af03c4a0181e9",
+    "url": "/weather/static/js/main.a8ca2c11.chunk.js"
+  },
+  {
+    "revision": "3bf48333ff6d73fc7b19",
+    "url": "/weather/static/js/runtime-main.c5407104.js"
+  }
+]);
--- src/service/Service.js
+++
@@ -1,6 +0,0 @@
-export async function getWeatherUpdates(url) {
-  let response = await fetch(url);
-  if (response.ok) {
-    return response.json();
-  }
-}
